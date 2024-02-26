e9f93efb42cdb6e65969a99ed4581df701715d6f
roguenomad <135940276+roguenomad@users.noreply.github.com>
2024-02-26 15:19:26 -0800

    js upload

---
+++ cam-popup.js
@@ -0,0 +1,12 @@
+// function openPopup() {
+//     document.getElementById("camera").style.width = "250px";
+//   }
+  
+//   function closePopup() {
+//     document.getElementById("camera").style.width = "0";
+//   }
+
+function openPopup() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
